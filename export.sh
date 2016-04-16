a3823984d406bd3e570d4336ae2dc02b1dd10fbc
modmuss50 <modmuss50@gmail.com>
2016-04-16 20:39:07 +0100

    Added export command

---
+++ export.sh
@@ -0,0 +1,2 @@
+wget -O exporter.jar http://maven.modmuss50.me/TechReborn/TechReborn-1.9/1.0.5.181/TechReborn-1.9-1.0.5.181-designer.jar
+java -jar exporter.jar -export ${WORKSPACE} /var/www/techreborn/manual/
